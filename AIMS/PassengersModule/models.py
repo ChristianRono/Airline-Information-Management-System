2b91660144ddf4adc6740cb098303460dacfff7c
Christian Kiprotich <kiprotichchristian@gmail.com>
2024-05-09 12:23:55 +0300

    Added the Pasenger model.

--- AIMS/PassengersModule/models.py
+++ AIMS/PassengersModule/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Passenger(models.Model):
+    surname = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.surname}, {self.name}"
