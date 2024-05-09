8490986dd3034c2d6fda684372f4cca701b854ab
Christian Kiprotich <kiprotichchristian@gmail.com>
2024-05-09 13:15:13 +0300

    Change the stops and passengers field in the Flight model and corrected the str function in the Airplane model.

--- AIMS/FlightModule/models.py
+++ AIMS/FlightModule/models.py
@@ -10,11 +10,11 @@ class Flight(models.Model):
     flight_number = models.IntegerField()
     origin = models.ForeignKey(City,related_name="origin",on_delete=models.DO_NOTHING)
     destination = models.ForeignKey(City,related_name="destination",on_delete=models.DO_NOTHING)
-    stops = models.ManyToManyField(City)
+    stops = models.ManyToManyField(City,blank=True)
     date = models.DateField()
     arrival_time = models.TimeField()
     departure_time = models.TimeField()
-    passengers = models.ManyToManyField(Passenger)
+    passengers = models.ManyToManyField(Passenger,blank=True)
     crew = models.ManyToManyField(Staff)
     airplane = models.ForeignKey(Airplane,on_delete=models.DO_NOTHING)
 
