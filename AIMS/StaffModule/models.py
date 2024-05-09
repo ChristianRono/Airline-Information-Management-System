3d3673692e24a919b95b90b0859b578109395b4e
Christian Kiprotich <kiprotichchristian@gmail.com>
2024-05-09 12:30:37 +0300

    Added the Staff model.

--- AIMS/StaffModule/models.py
+++ AIMS/StaffModule/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Staff(models.Model):
+    employee_number = models.IntegerField()
+    surname = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    salary = models.IntegerField()
+    CHOICES = (
+        ('a','A'),
+        ('b','B'),
+        ('c','C'),
+    )
+    rating = models.CharField(max_length=10,choices=CHOICES)
+    is_pilot = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.surname}, {self.name}"
