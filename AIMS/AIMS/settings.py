845d1f674612ce93e3416f55b1ea9c49b12ba7e7
Christian Kiprotich <kiprotichchristian@gmail.com>
2024-05-09 12:43:58 +0300

    Changed the Settings file to add the modules to the apps list

--- AIMS/AIMS/settings.py
+++ AIMS/AIMS/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'AirplaneModule',
+    'CitiesModule',
+    'FlightModule',
+    'PassengersModule',
+    'StaffModule',
 ]
 
 MIDDLEWARE = [
