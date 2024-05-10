20296da4d78c8c75b2d1f6852c942d47516f5263
Christian Kiprotich <kiprotichchristian@gmail.com>
2024-05-10 13:54:54 +0300

    Changed the ALLOWED_HOSTS variable.

--- AIMS/settings.py
+++ AIMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+nvpgd&wi#3#wba#mix^^p*+mtd!+t&+8moe361x!u9h+&8ft3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
