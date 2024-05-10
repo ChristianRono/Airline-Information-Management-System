609bf95f61f8ad912fce2c28b0151c3d29b3958d
Christian Kiprotich <kiprotichchristian@gmail.com>
2024-05-10 13:07:03 +0300

    Final Commit for 10 May 2024

--- StaffModule/views.py
+++ StaffModule/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 
 from StaffModule.models import Staff
 from StaffModule.forms import StaffForm
@@ -6,7 +7,7 @@ from StaffModule.forms import StaffForm
 # Create your views here.
 
 def index(request):
-    staffs = Staff.objects.all()
+    staffs = Staff.objects.all().order_by('employee_number')
 
     return render(request,'staff_listview.html',{'staffs':staffs})
 
@@ -14,6 +15,21 @@ def add_new(request):
     if request.method == "POST":
         form = StaffForm(request.POST)
         if form.is_valid():
+            is_pilot = form.cleaned_data['is_pilot']
+            rating = form.cleaned_data['rating']
+
+            if is_pilot:
+                if rating:
+                    form.save()
+                    return redirect("staff listview")
+                else:
+                    messages.warning(request, "You can't add a pilot without a rating!")
+                    return render(request, 'staff_new.html',{'form':form})
+            else:
+                if rating:
+                    messages.warning(request, "Normal crew members don't have a rating!")
+                    return render(request, 'staff_new.html',{'form':form})
+
             form.save()
             return redirect("staff listview")
     else:
@@ -25,6 +41,21 @@ def detail_view(request,id):
         staff =Staff.objects.get(id=request.POST['id'])
         form = StaffForm(request.POST,instance=staff)
         if form.is_valid():
+            is_pilot = form.cleaned_data['is_pilot']
+            rating = form.cleaned_data['rating']
+
+            if is_pilot:
+                if rating:
+                    form.save()
+                    return redirect("staff detailview",id=staff.pk)
+                else:
+                    messages.warning(request, "You can't add a pilot without a rating!")
+                    return render(request, 'staff_detail.html',{'form':form})
+            else:
+                if rating:
+                    messages.warning(request, "Normal crew members don't have a rating!")
+                    return render(request, 'staff_detail.html',{'form':form})
+
             form.save()
             return redirect("staff detailview",id=staff.pk)
     else:
